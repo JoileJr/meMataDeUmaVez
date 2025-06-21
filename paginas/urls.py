508928f506f05753c3076cf18820f83b84ac95c1
JoileJr <joilejunior2@gmail.com>
2025-06-21 17:40:11 -0300

    criando tela de sobre para exibiçaõ de diagramas

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     # Crie suas urls para as views
     path("", IndexView.as_view(), name="index"),
+    path("sobre", SobreView.as_view(), name="index"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -2,4 +2,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'paginas/index.html'
+    template_name = 'paginas/modelo.html'
+    
+class SobreView(TemplateView):
+    template_name = 'paginas/sobre.html'
