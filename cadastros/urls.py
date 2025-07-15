467cdbc899c3d3c4490ce213dd8a4b2ffeb505e0
joilejunior <j.junior@nextage.com.br>
2025-07-15 17:00:56 -0300

    barbeiros crud

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from .views import EnderecoCreateView, BarbeariaCreateView, BarbeariaUpdateView, EnderecoUpdateView, EnderecoDeleteView, BarbeariaDeleteView, BarbeariaListView
+from .views import AdministradorCreateView, AdministradorUpdateView, AdministradorDeleteView, AdministradorListView
+from .views import BarbeiroCreateView, BarbeiroUpdateView, BarbeiroDeleteView, BarbeiroListView
 
 urlpatterns = [
     path('cadastrar/endereco/', EnderecoCreateView.as_view(), name='cadastrar-endereco'),
@@ -13,4 +15,14 @@ urlpatterns = [
     path('excluir/barbearia/<int:pk>/', BarbeariaDeleteView.as_view(), name='excluir-barbearia'),
     
     path('listar/barbearias/', BarbeariaListView.as_view(), name='barbearia-lista' ),
+    
+    path('cadastrar/administrador/', AdministradorCreateView.as_view(), name='cadastrar-admin'),
+    path('editar/administrador/<int:pk>/', AdministradorUpdateView.as_view(), name='editar-admin'),
+    path('excluir/administrador/<int:pk>/', AdministradorDeleteView.as_view(), name='excluir-admin'),
+    path('listar/administradores/', AdministradorListView.as_view(), name='administrador-lista'),
+    
+    path('cadastrar/barbeiro/', BarbeiroCreateView.as_view(), name='cadastrar-barbeiro'),
+    path('editar/barbeiro/<int:pk>/', BarbeiroUpdateView.as_view(), name='editar-barbeiro'),
+    path('excluir/barbeiro/<int:pk>/', BarbeiroDeleteView.as_view(), name='excluir-barbeiro'),
+    path('listar/barbeiros/', BarbeiroListView.as_view(), name='barbeiro-lista'),
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -12,19 +12,15 @@ class EnderecoCreateView(CreateView):
     fields = ['rua', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
-    
-class BarbeariaCreateView(CreateView):
-    model = Barbearia
-    fields = ['nome_fantasia', 'razao_social', 'cnpj', 'telefone', 'email', 'endereco']
-    template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
-    
-## Update Views ##
-    
 class EnderecoUpdateView(UpdateView):
     model = Endereco
     fields = ['rua', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep']
     template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+    
+class EnderecoDeleteView(DeleteView):
+    model = Endereco
+    template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('index')
     
 class BarbeariaUpdateView(UpdateView):
@@ -32,22 +28,64 @@ class BarbeariaUpdateView(UpdateView):
     fields = ['nome_fantasia', 'razao_social', 'cnpj', 'telefone', 'email', 'endereco']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
-        
-## Delete Views ##
 
-class EnderecoDeleteView(DeleteView):
-    model = Endereco
-    template_name = 'cadastros/form-excluir.html'
+class BarbeariaCreateView(CreateView):
+    model = Barbearia
+    fields = ['nome_fantasia', 'razao_social', 'cnpj', 'telefone', 'email', 'endereco']
+    template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
-
+    
 class BarbeariaDeleteView(DeleteView):
     model = Barbearia
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('index')
     
-## List Views ##
-
 class BarbeariaListView(ListView):
     model = Barbearia
     template_name = 'listas/barbearia.html'
     context_object_name = 'barbearias'
+    
+## Administrador Views ##
+class AdministradorCreateView(CreateView):
+    model = Administrador
+    fields = ['nome', 'email', 'telefone', 'cargo', 'barbearia']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class AdministradorUpdateView(UpdateView):
+    model = Administrador
+    fields = ['nome', 'email', 'telefone', 'cargo', 'barbearia']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class AdministradorDeleteView(DeleteView):
+    model = Administrador
+    template_name = 'cadastros/form-excluir.html'
+    success_url = reverse_lazy('index')
+
+class AdministradorListView(ListView):
+    model = Administrador
+    template_name = 'listas/administrador.html'
+    context_object_name = 'administradores'
+    
+class BarbeiroCreateView(CreateView):
+    model = Barbeiro
+    fields = ['nome', 'telefone', 'especialidade', 'ativo', 'barbearia']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('barbeiro-lista')
+    
+class BarbeiroUpdateView(UpdateView):
+    model = Barbeiro
+    fields = ['nome', 'telefone', 'especialidade', 'ativo', 'barbearia']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('barbeiro-lista')
+
+class BarbeiroDeleteView(DeleteView):
+    model = Barbeiro
+    template_name = 'cadastros/form-excluir.html'
+    success_url = reverse_lazy('barbeiro-lista')
+    
+class BarbeiroListView(ListView):
+    model = Barbeiro
+    template_name = 'listas/barbeiro.html'
+    context_object_name = 'barbeiros'
