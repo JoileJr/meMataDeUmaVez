566592a51e049e3d2686fc20792a63b2d260a3f3
joilejunior <j.junior@nextage.com.br>
2025-07-18 10:03:24 -0300

    autocadastro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from .views import UsuarioCreateView
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path('cadastro/', UsuarioCreateView.as_view(), name='usuario-cadastro'),
     
     path("alterar-senha/", PasswordChangeView.as_view(
         template_name="usuarios/form.html",
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User, Group
+from django.urls import reverse_lazy
+from .forms import UsuarioForm
+from django.shortcuts import get_object_or_404
 # Create your views here.
+    
+class UsuarioCreateView(CreateView):
+    template_name = 'usuarios/autocadastro.html'
+    form_class = UsuarioForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        grupo = get_object_or_404(Group, name="Adminstrador")
+        url = super().form_valid(form)
+        self.object.groups.add(grupo)
+        self.object.save()
+
+        return url
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['titulo'] = "Registro de novo usuário"
+        context['botao'] = "Cadastrar"
+
+        return context
